a1e896a40aed02fadcf7eb69c63ec1c4a9d8c945
Natalia Vladimirova <dochka.tankista@gmail.com>
2016-03-19 18:42:55 +0300

    Task 2: delegates were replaced by interfaces, nunit-tests were added.

---
+++ Task2.NUnitTests/JaggedArrayTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task2.NUnitTests
+{
+    [TestFixture]
+    public class JaggedArrayTests
+    {
+        [TestCase(ExpectedException = typeof(ArgumentNullException))]
+        public void SortJaggedArray_NullArray()
+        {
+            JaggedArray.SortJaggedArray(null, new AscendingSortOrder(), new RowSumSortFeature());
+        }
+
+        [TestCase(ExpectedException = typeof(ArgumentNullException))]
+        public void SortJaggedArray_NullSortOrder()
+        {
+            JaggedArray.SortJaggedArray(new int[][] { new int[] { 1 } }, null, new RowSumSortFeature());
+        }
+
+        [TestCase(ExpectedException = typeof(ArgumentNullException))]
+        public void SortJaggedArray_NullSortFeature()
+        {
+            JaggedArray.SortJaggedArray(new int[][] { new int[] { 1 } }, new AscendingSortOrder(), null);
+        }
+
+        [TestCase]
+        public void SortJaggedArray_AscendingOrder_RowSumSortFeature()
+        {
+            //Arrange
+            int[][] arrangeArray = new int[][]
+            {
+                new int[] { },
+                new int[] { 2, 3, 5 },
+                new int[] { 2, 4, 7, 5 },
+                new int[] { 20 },
+                new int[] { 1, 4, 8, 100 },
+                null,
+                null,
+                null,
+                null
+            };
+
+            //Act
+            int[][] actArray = new int[][]
+            {
+                new int[] { 2, 3, 5 },
+                new int[] { 1, 4, 8, 100 },
+                null,
+                new int[] { 20 },
+                null,
+                null,
+                new int[] { 2, 4, 7, 5 },
+                new int[] { },
+                null
+            };
+
+            JaggedArray.SortJaggedArray(actArray, new AscendingSortOrder(), new RowSumSortFeature());
+
+            //Assert
+            Assert.AreEqual(arrangeArray, actArray);
+        }
+
+        [TestCase]
+        public void SortJaggedArray_DescendingOrder_MaxElemModuleSortFeature()
+        {
+            //Arrange
+            int[][] arrangeArray = new int[][]
+            {
+                new int[] { },
+                new int[] { 1, 4, 8, -100 },
+                new int[] { 20 },
+                new int[] { 2, 4, -7, 5 },
+                new int[] { 2, 3, 5 },
+                null,
+                null,
+                null,
+                null
+            };
+
+            //Act
+            int[][] actArray = new int[][]
+            {
+                new int[] { 2, 3, 5 },
+                new int[] { 1, 4, 8, -100 },
+                null,
+                new int[] { 20 },
+                null,
+                null,
+                new int[] { 2, 4, -7, 5 },
+                new int[] { },
+                null
+            };
+
+            JaggedArray.SortJaggedArray(actArray, new DescendingSortOrder(), new MaxElemModuleSortFeature());
+
+            //Assert
+            Assert.AreEqual(arrangeArray, actArray);
+        }
+
+
+    }
+}
--- Task2/JaggedArray.cs
+++ Task2/JaggedArray.cs
@@ -6,83 +6,109 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    public delegate int SortingMethodDelegate(int x, int y);
-    public delegate int SortingByDelegate(int[] array);
+    #region interfaces
+
+    public interface ISortOrder
+    {
+        int SortingMethod(int x, int y);
+    }
+
+    public interface ISortFeature
+    {
+        int FindSortFeature(int[] array);
+    }
+
+    #endregion
+
+    #region array sorting
 
     public static class JaggedArray
     {
-        public static void SortJaggedArray(int[][] array, SortingMethodDelegate sortingMethod, SortingByDelegate sortingBy)
+        public static void SortJaggedArray(int[][] array, ISortOrder sortOrder, ISortFeature sortFeature)
         {
-            if (array == null)
-                throw new ArgumentNullException();
+            if (array == null) { throw new ArgumentNullException(); }
 
-            if (sortingMethod == null)
-                sortingMethod = SortingMethod.Ascending;
+            if (sortOrder == null) { throw new ArgumentNullException(); }
 
-            if (sortingBy == null)
-                sortingBy = SortingBy.RowSum;
+            if (sortFeature == null) { throw new ArgumentNullException(); }
 
             for (int i = 0; i < array.Length - 1; i++)
+            {
                 for (int j = 0; j < array.Length - i - 1; j++)
+                {
                     if ((array[j] == null) || (array[j + 1] != null && array[j + 1].Length == 0) ||
                         (array[j + 1] != null && array[j].Length != 0 &&
-                        sortingMethod(sortingBy(array[j]), sortingBy(array[j + 1])) > 0))
+                        sortOrder.SortingMethod(sortFeature.FindSortFeature(array[j]), sortFeature.FindSortFeature(array[j + 1])) > 0))
                     {
-                        int[] temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
+                        Swap(ref array[j], ref array[j + 1]);
                     }
+                }
+            }
+
         }
 
+        private static void Swap(ref int[] firstItem, ref int[] secondItem)
+        {
+            int[] temp = firstItem;
+            firstItem = secondItem;
+            secondItem = temp;
+        }
     }
 
-    public static class SortingMethod
-    {
-        public static SortingMethodDelegate Ascending { get; } = AscendingMethod;
-        public static SortingMethodDelegate Descending { get; } = DescendingMethod;
+    #endregion
 
-        private static int AscendingMethod(int x, int y)
+    #region sort order
+
+    public class AscendingSortOrder : ISortOrder
+    {
+        // ascending method
+        public int SortingMethod(int x, int y)
         {
             return x > y ? 1 : x == y ? 0 : -1;
         }
+    }
 
-        private static int DescendingMethod(int x, int y)
+    public class DescendingSortOrder : ISortOrder
+    {
+        // descending method
+        public int SortingMethod(int x, int y)
         {
             return x < y ? 1 : x == y ? 0 : -1;
         }
     }
 
-    public static class SortingBy
-    {
-        public static SortingByDelegate RowSum { get; } = FindRowSum;
-        public static SortingByDelegate Max { get; } = FindMax;
-        public static SortingByDelegate Min { get; } = FindMin;
+    #endregion
 
-        private static int FindRowSum(int[] array)
+    #region sort features
+
+    public class RowSumSortFeature : ISortFeature
+    {
+        // sort feature: row sum
+        public int FindSortFeature(int[] array)
         {
             int result = 0;
             for (int i = 0; i < array.Length; i++)
                 result += array[i];
             return result;
         }
+    }
 
-        private static int FindMax(int[] array)
+    public class MaxElemModuleSortFeature : ISortFeature
+    {
+        // sort feature: max module of element
+        public int FindSortFeature(int[] array)
         {
-            int max = array[0];
+            int max = Math.Abs(array[0]);
             for (int i = 1; i < array.Length; i++)
-                if (array[i] > max)
-                    max = array[i];
+            {
+                if (Math.Abs(array[i]) > max)
+                {
+                    max = Math.Abs(array[i]);
+                }
+            }
             return max;
-        }
-
-        private static int FindMin(int[] array)
-        {
-            int min = array[0];
-            for (int i = 1; i < array.Length; i++)
-                if (array[i] < min)
-                    min = array[i];
-            return min;
         }
     }
 
+    #endregion
 }
